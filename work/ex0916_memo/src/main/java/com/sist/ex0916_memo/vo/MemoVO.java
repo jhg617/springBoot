40c5d4db02cf1072fb3061004b20a71d70eed193
jhg617 <gksruf2233@gmail.com>
2025-09-16 15:22:42 +0900

    SpringBoot server for communicating with NextJS

---
+++ work/ex0916_memo/src/main/java/com/sist/ex0916_memo/vo/MemoVO.java
@@ -0,0 +1,16 @@
+package com.sist.ex0916_memo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemoVO {
+
+    private String idx;
+    private String writer, content, reg_date;
+}
